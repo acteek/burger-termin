7bfbf22d2a4c2292dfc3b431b075d466795bd049
Sergey Ryazanov <sergey.ryazanov@nordigy.ru>
2023-05-09 22:45:18 +0200

    update

--- src/main/scala/com/github/acteek/burgertermin/Main.scala
+++ src/main/scala/com/github/acteek/burgertermin/Main.scala
@@ -6,7 +6,7 @@ import org.http4s.ember.client.EmberClientBuilder
 
 object Main extends IOApp.Simple {
   def run: IO[Unit] = (for {
-    token   <- Resource.eval(IO(sys.env.getOrElse("BOT_TOKEN", "5678047616:AAGks3xy0_JRkmwxuCZjCNJEDHqVAfwzveY")))
+    token   <- Resource.eval(IO(sys.env.getOrElse("BOT_TOKEN", "")))
     client  <- EmberClientBuilder.default[IO].build
     store   <- Resource.eval(SubscriptionStore.memory)
     sendQ   <- Resource.eval(Queue.unbounded[IO, Subscription])
